e53fb872a943cafebe01bbff4728bd02db6102d5
Mohamed Augustin Sesay <sesaymohamedaugustin@gmail.com>
2022-08-08 12:29:10 +0000

    Printf

--- _printf.c
+++ _printf.c
@@ -11,8 +11,21 @@ int (*check_format(const char *format))(va_list)
 	int i = 0;
 	print_t p[] = {
 		{"c", print_c},
-		{"s", print_s}
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"b", print_b},
+		{"u", print_u},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
 	};
+
 	for (; p[i].t != NULL; i++)
 	{
 		if (*(p[i].t) == *format)
