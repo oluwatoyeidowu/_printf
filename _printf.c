3adeb9934b0a2fec2c4a2d4011a71dd9fa172259
Oluwatoye Idowu <oagbonjinmi@gmail.com>
2022-08-08 10:52:35 -0700

    Fix

--- _printf.c
+++ _printf.c
@@ -1,84 +1,50 @@
 #include "main.h"
-#include <stdarg.h>
+#include <stdlib.h>
 
 /**
- * check_format - checks if there is a valid format specifier
- * @format: possible valid format specifier
- * Return: pointer to valid function or NULL
+ * print_p - A function that prints an address
+ * @p: address to print
+ * Return: number of printed characters
  */
-int (*check_format(const char *format))(va_list)
+int print_p(va_list p)
 {
-	int i = 0;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"b", print_b},
-		{"u", print_u},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
+        int counter = 0;
+        unsigned int a[16];
+        unsigned int i = 0, sum = 0;
+        unsigned long n, m = 1152921504606846976;
+        char *str = "(nil)";
 
-	for (; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-			break;
-	}
-	return (p[i].f);
-}
+        n = va_arg(p, unsigned long);
+        if (n == 0)
+        {
+                for (; str[i]; i++)
+                {
+                        _putchar(str[i]);
+                        counter++;
+                }
+                return (counter);
+        }
+        _putchar('0');
+        _putchar('x');
+        counter = 2;
 
-/**
- * _printf - function for format printing
- * @format: list of arguments to printing
- * Return: Number of characters to printing
- */
-int _printf(const char *format, ...)
-{
-	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0, counter = 0;
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(ap, format);
-	while (format && format[i])
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			counter++;
-			i++;
-			continue;
-		}
-		else
-		{
-			if (format[i + 1] == '%')
-			{
-				_putchar('%');
-				counter++;
-				i += 2;
-				continue;
-			}
-			else
-			{
-				f = check_format(&format[i + 1]);
-				if (f == NULL)
-					return (-1);
-				i += 2;
-				counter += f(ap);
-				continue;
-			}
-		}
-		i++;
-	}
-	va_end(ap);
-	return (counter);
+        a[0] = n / m;
+        for (i = 1; i < 16; i++)
+        {
+                m /= 16;
+                a[i] = (n / m) % 16;
+        }
+        for (i = 0; i < 16; i++)
+        {
+                sum += a[i];
+                if (sum || i == 15)
+                {
+                        if (a[i] < 10)
+                                _putchar('0' + a[i]);
+                        else
+                                _putchar('0' + ('a' - ':') + a[i]);
+                        counter++;
+                }
+        }
+        return (counter);
 }
